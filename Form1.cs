ac9c53eda36dac57ff9d97d23cd09640e8c73ffa
Kyle McBride <Kyle@MordeKyle.com>
2014-03-06 12:34:08 -0600

    Initial Commit
    Creating Repository

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*Program: Program Name
+  Author: Kyle McBride A02609917
+  Date: MM/DD/YYYY
+  Description: Description
+                                          ***SEE CHANGELOG IN SOLUTION DIRECTORY***							*/
+
+namespace Rock_Paper_Scissors_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
