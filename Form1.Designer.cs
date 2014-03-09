1902dd7f2242512e2e606773e31b25a623c668c6
Kyle McBride <Kyle@MordeKyle.com>
2014-03-09 05:11:32 -0500

    Version: 1.0 Building Robin
    03/09/2014 @ 05:00
    •declared variables
    winConstant
    loseConstant
    drawConstant
    resultHolder
    userInputConstant
    computerChoice
    winString
    loseString
    drawString
    paperString
    rockString
    scissorsString
    •built funtions
    assignCompChoice
    calculateResults
    rockSelected
    paperSelected
    scissorsSelected
    playButton_Click
    •put everything on the form
    **considering a wait before displaying the results. Also adding a "One
    Moment" in the label while
    waiting. That way, if the results are the same, the user sees a change
    in the output.
    ***Everything seems to be working. I'm either starting to understand all
    of this, or I'm lucky.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.inputGroubBox = new System.Windows.Forms.GroupBox();
+            this.rockRad = new System.Windows.Forms.RadioButton();
+            this.paperRad = new System.Windows.Forms.RadioButton();
+            this.scissorsRad = new System.Windows.Forms.RadioButton();
+            this.playButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.computerChoiceLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.inputGroubBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // inputGroubBox
+            // 
+            this.inputGroubBox.Controls.Add(this.rockRad);
+            this.inputGroubBox.Controls.Add(this.paperRad);
+            this.inputGroubBox.Controls.Add(this.scissorsRad);
+            this.inputGroubBox.Location = new System.Drawing.Point(5, 5);
+            this.inputGroubBox.Name = "inputGroubBox";
+            this.inputGroubBox.Size = new System.Drawing.Size(148, 100);
+            this.inputGroubBox.TabIndex = 0;
+            this.inputGroubBox.TabStop = false;
+            this.inputGroubBox.Text = "Rock, Paper, or Scissors?";
+            // 
+            // rockRad
+            // 
+            this.rockRad.AutoSize = true;
+            this.rockRad.Location = new System.Drawing.Point(6, 31);
+            this.rockRad.Name = "rockRad";
+            this.rockRad.Size = new System.Drawing.Size(51, 17);
+            this.rockRad.TabIndex = 1;
+            this.rockRad.TabStop = true;
+            this.rockRad.Text = "Rock";
+            this.rockRad.UseVisualStyleBackColor = true;
+            // 
+            // paperRad
+            // 
+            this.paperRad.AutoSize = true;
+            this.paperRad.Location = new System.Drawing.Point(6, 54);
+            this.paperRad.Name = "paperRad";
+            this.paperRad.Size = new System.Drawing.Size(53, 17);
+            this.paperRad.TabIndex = 2;
+            this.paperRad.TabStop = true;
+            this.paperRad.Text = "Paper";
+            this.paperRad.UseVisualStyleBackColor = true;
+            // 
+            // scissorsRad
+            // 
+            this.scissorsRad.AutoSize = true;
+            this.scissorsRad.Location = new System.Drawing.Point(6, 77);
+            this.scissorsRad.Name = "scissorsRad";
+            this.scissorsRad.Size = new System.Drawing.Size(64, 17);
+            this.scissorsRad.TabIndex = 3;
+            this.scissorsRad.TabStop = true;
+            this.scissorsRad.Text = "Scissors";
+            this.scissorsRad.UseVisualStyleBackColor = true;
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(179, 128);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(75, 23);
+            this.playButton.TabIndex = 4;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(196, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "The Computer\'s Choice:";
+            // 
+            // computerChoiceLabel
+            // 
+            this.computerChoiceLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.computerChoiceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.computerChoiceLabel.Location = new System.Drawing.Point(322, 33);
+            this.computerChoiceLabel.Name = "computerChoiceLabel";
+            this.computerChoiceLabel.Size = new System.Drawing.Size(100, 23);
+            this.computerChoiceLabel.TabIndex = 6;
+            this.computerChoiceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(271, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Results:";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(322, 77);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(100, 23);
+            this.resultLabel.TabIndex = 8;
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(434, 163);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.computerChoiceLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.inputGroubBox);
+            this.Name = "Form1";
+            this.Text = "Rock Paper Scissors";
+            this.inputGroubBox.ResumeLayout(false);
+            this.inputGroubBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox inputGroubBox;
+        private System.Windows.Forms.RadioButton rockRad;
+        private System.Windows.Forms.RadioButton paperRad;
+        private System.Windows.Forms.RadioButton scissorsRad;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label computerChoiceLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label resultLabel;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,19 +7,128 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-/*Program: Program Name
+/*Program: Rock Paper Scissors Game
   Author: Kyle McBride A02609917
-  Date: MM/DD/YYYY
-  Description: Description
-                                          ***SEE CHANGELOG IN SOLUTION DIRECTORY***							*/
+  Date: 03/09/2014
+  Description: Application that lets the user play the game of Rock, Paper, Scissors against the computer.
+                                          ***SEE CHANGELOG IN SOLUTION DIRECTORY***				*/
 
 namespace Rock_Paper_Scissors_Game
 {
     public partial class Form1 : Form
     {
+        //declaring variables
+        private const int winConstant = 1;
+        private const int loseConstant = -1;
+        private const int drawConstant = 0;
+        private int resultHolder;
+        private const int userInputConstant = 2;
+        private int computerChoice;
+        private string winString = "You Won!";
+        private string loseString = "You lost";
+        private string drawString = "Draw";
+        private string paperString = "Paper";
+        private string rockString = "Rock";
+        private string scissorsString = "Scissors";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void assignCompChoice()
+        {
+            Random rand = new Random();
+            computerChoice = rand.Next(3) + 1;
+        }
+
+        private void calculateResults()
+        {
+            resultHolder = userInputConstant - computerChoice;
+        }
+
+        private void rockSelected()
+        {
+            if (resultHolder == 1)
+            {
+                computerChoiceLabel.Text = scissorsString;
+                resultLabel.Text = winString;
+            }
+            else if (resultHolder == -1)
+            {
+                computerChoiceLabel.Text = paperString;
+                resultLabel.Text = drawString;
+            }
+            else
+            {
+                computerChoiceLabel.Text = rockString;
+                resultLabel.Text = drawString;
+            }
+        }
+
+        private void paperSelected()
+        {
+            if (resultHolder == 1)
+            {
+                computerChoiceLabel.Text = rockString;
+                resultLabel.Text = winString;
+            }
+            else if (resultHolder == -1)
+            {
+                computerChoiceLabel.Text = scissorsString;
+                resultLabel.Text = loseString;
+            }
+            else
+            {
+                computerChoiceLabel.Text = paperString;
+                resultLabel.Text = drawString;
+            }
+        }
+
+        private void scissorsSelected()
+        {
+            if (resultHolder == 1)
+            {
+                computerChoiceLabel.Text = paperString;
+                resultLabel.Text = winString;
+            }
+            else if (resultHolder == -1)
+            {
+                computerChoiceLabel.Text = rockString;
+                resultLabel.Text = loseString;
+            }
+            else
+            {
+                computerChoiceLabel.Text = scissorsString;
+                resultLabel.Text = drawString;
+            }
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                assignCompChoice();
+
+                calculateResults();
+
+                if (rockRad.Checked)
+                {
+                    rockSelected();
+                }
+                else if (paperRad.Checked)
+                {
+                    paperSelected();
+                }
+                else
+                {
+                    scissorsSelected();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
